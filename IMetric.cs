fdd4afd18638d54dbdbbb77a13ffb54710f89dc6
Nigel Delaney <ndelaney@broadinstitute.org>
2014-02-09 23:50:22 -0500

    Initial Code Commit
    Start of code

---
+++ IMetric.cs
@@ -0,0 +1,32 @@
+﻿using Bio.IO.SAM;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploidulator
+{
+    /// <summary>
+    /// An IMetric calculates various metric values from a list of SAMAlignedSequences
+    /// </summary>
+    public interface IMetric
+    {
+        /// <summary>
+        /// String of tab-separated values for writing to file by MetricFormatter.
+        /// </summary>
+        string ToString();
+
+        /// <summary>
+        /// Calculate metric values from the given list of sequences.
+        /// </summary>
+        void Calculate(Collection<SAMAlignedSequence> clusterSequences);
+
+        /// <summary>
+        /// Reset all values to null and lists to empty.
+        /// </summary>
+        void Reset();
+
+    }
+}
---
+++ IMetricHandler.cs
@@ -0,0 +1,45 @@
+﻿using Bio.IO.SAM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploidulator
+{
+    /// <summary>
+    /// An IMetricHandler receives a set of aligned sequences or an individual sequence
+    /// and performs operations on these sequence/s
+    /// </summary>
+    public interface IMetricHandler : IDisposable
+    {
+
+        /// <summary>
+        /// Add a sequence.
+        /// </summary>
+        /// <param name="sequence">A sequence.</param>
+        /// <returns>True if successfully added, false otherwise</returns>
+        bool Add(SAMAlignedSequence sequence);
+
+        /// <summary>
+        /// Add a set of sequences.
+        /// THIS IS NOT ACTUALLY CALLED IN BAMPARSER, BUT SEEMS A LOGICAL INCLUSION - AW
+        /// </summary>
+        /// <param name="sequences">A set of sequences.</param>
+        /// <returns>True if successfully added, false otherwise</returns>
+        bool AddRange(IEnumerable<SAMAlignedSequence> sequences);
+
+        /// <summary>
+        /// Process sequences (may process all, or a selection thereof).
+        /// ALSO NOT CALLED BY BAMPARSER. IS THIS A LOGICAL INCLUSION FOR A MORE FUNCTIONAL
+        /// INTERFACE, OR SHOULD IT BE REMOVED? - AW
+        /// </summary>
+        void ProcessSequences();
+
+        /// <summary>
+        /// Process any/all remaining sequences.
+        /// </summary>
+        void SetComplete();
+
+    }
+}
